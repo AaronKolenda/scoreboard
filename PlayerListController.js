635b0693856b5c150dd9b5bb83fff8a0250e35a8
AaronKolenda <kolenda.a@gmail.com>
2015-03-27 12:23:51 -0400

    renaming files

--- PlayerListController.js
+++ PlayerListController.js
@@ -7,9 +7,7 @@ angular.module("playerListController", [])
 	
 		$http.get('http://scoreboardgames.herokuapp.com/api/v1/players?access_token=c3da9194b4fde0c768c93b6fdd8794af13261d2b6607d40f').
 		  success(function(data) {
-		    $scope.playerListData = data;
-		    console.log($scope.playerListData);
-		    console.log($scope.playerListData[0].player.email);    
+		    $scope.playerListData = data;    
 		  }).
 		  error(function(data, status, headers, config) {
 		    console.log("failed");
