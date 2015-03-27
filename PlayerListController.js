e546c2171a6b4d11d529793273955a8664645383
AaronKolenda <kolenda.a@gmail.com>
2015-03-27 12:00:37 -0400

    creating player list

--- PlayerListController.js
+++ PlayerListController.js
@@ -2,13 +2,19 @@ angular.module("playerListController", [])
 .controller("PlayerListController", ["$scope", "$http", function($scope, $http){
 
 	$scope.playerListData; 
+
+	$scope.listPlayers = function() {
 	
-	$http.get('http://scoreboardgames.herokuapp.com/api/v1/players?access_token=c3da9194b4fde0c768c93b6fdd8794af13261d2b6607d40f').
-	  success(function(data) {
-	    $scope.playerListData = data;
-	  }).
-	  error(function(data, status, headers, config) {
-	    console.log("failed");
-  	});
+		$http.get('http://scoreboardgames.herokuapp.com/api/v1/players?access_token=c3da9194b4fde0c768c93b6fdd8794af13261d2b6607d40f').
+		  success(function(data) {
+		    $scope.playerListData = data;
+		    console.log($scope.playerListData);
+		    console.log($scope.playerListData[0].player.email);    
+		  }).
+		  error(function(data, status, headers, config) {
+		    console.log("failed");
+	  	});
+
+	}
 
 }])
--- gameListController.js
+++ gameListController.js
@@ -2,6 +2,8 @@ angular.module("gameListController", [])
 .controller("GameListController", ["$scope", "$http", function($scope, $http){
 
 	$scope.gameListData; 
+
+	$scope.listGames = function() {
 	
 	$http.get('http://scoreboardgames.herokuapp.com/api/v1/games?access_token=c3da9194b4fde0c768c93b6fdd8794af13261d2b6607d40f').
 	  success(function(data) {
@@ -12,43 +14,6 @@ angular.module("gameListController", [])
 	    console.log("failed");
   	});
 
-
-
-	/*$scope.gameListData = [
-	{
-	game: {
-	id: 2,
-	name: "Battleship",
-	created_at: "2015-03-26T21:16:36.394Z",
-	updated_at: "2015-03-26T21:16:36.394Z"
-	},
-	players: [ ]
-	},
-	{
-	game: {
-	id: 1,
-	name: "War",
-	created_at: "2015-03-26T19:42:17.607Z",
-	updated_at: "2015-03-26T19:42:17.607Z"
-	},
-	players: [
-	{
-	id: 1,
-	email: "alex@example.com",
-	created_at: "2015-03-26T19:42:47.390Z",
-	updated_at: "2015-03-26T19:42:47.390Z"
-	},
-	{
-	id: 2,
-	email: "bob@example.com",
-	created_at: "2015-03-26T19:42:53.367Z",
-	updated_at: "2015-03-26T19:42:53.367Z"
 	}
-	]
-	}
-	]
-
-	console.log($scope.gameListData)*/
-
 
 }])
